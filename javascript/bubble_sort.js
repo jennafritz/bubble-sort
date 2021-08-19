611f465971546f53d662b3ec8059f119fed440d9
jennafritz <jennafritz24@gmail.com>
2021-08-19 17:04:15 -0400

    Completed JS

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,5 +1,20 @@
 function bubbleSort(arr) {
-  // type your code here
+  
+  let swaps = 0
+
+  arr.forEach((element, index) => {
+    if(element > arr[index + 1]){
+      [arr[index], arr[index + 1]] = [arr[index + 1], arr[index]]
+      swaps += 1
+    }
+  })
+
+  if(swaps === 0){
+    return arr
+  } else {
+    return bubbleSort(arr)
+  }
+
 }
 
 if (require.main === module) {
@@ -7,17 +22,20 @@ if (require.main === module) {
   console.log("Expecting: [1, 2, 3, 4]");
   console.log("=>", bubbleSort([3, 2, 1, 4]));
 
-  console.log("");
+  console.log("Expecting: [-1, 5, 7, 12]");
+  console.log("=>", bubbleSort([7, 12, -1, 5]));
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", bubbleSort([1, 2, 3]));
 
-  console.log("");
+  console.log("Expecting: [10, 20, 30 70]");
+  console.log("=>", bubbleSort([20, 70, 30, 10]));
 
   console.log("Expecting: []");
   console.log("=>", bubbleSort([]));
 
-  console.log("");
+  console.log("Expecting: [-82, -47, 13, 109]");
+  console.log("=>", bubbleSort([-47, 109, -82, 13]));
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", bubbleSort([2, 3, 1]));
